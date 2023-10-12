9a01a614a3512bcd1e29c3412ab1bb361e7e855b
NX-ETO <91761316+NX-ETO@users.noreply.github.com>
2023-10-12 08:08:46 -0500

    Update training.py

--- model_definitions/Audiencias_Educacion/model_modules/training.py
+++ model_definitions/Audiencias_Educacion/model_modules/training.py
@@ -32,6 +32,13 @@ def train(context: ModelContext, **kwargs):
                                oob_score=context.hyperparams["oob_score"])
 
     Morf=modelo.fit(X_train,y_train)
+
+    importancia_predictores = pd.DataFrame(
+                        {'predictor': X_train.columns,
+                         'importancia': modelo.feature_importances_}
+                        )
+    importancia_predictores.sort_values('importancia', ascending=False)
+    
     modelo_calibrado = CalibratedClassifierCV(Morf, cv=5, method='isotonic')
     modelo_calibrado = modelo_calibrado.fit(X_train,y_train)
     joblib.dump(modelo_calibrado,f"{context.artifact_output_path}/model.joblib")
@@ -43,5 +50,6 @@ def train(context: ModelContext, **kwargs):
         features=feature_names,
         targets=[target_name],
         categorical=[target_name],
+        feature_importance=importancia_predictores,
         context=context
     )
